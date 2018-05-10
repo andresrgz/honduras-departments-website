3f443c022f7d3d82edbfbdf91e3c95ad858a3a2c
astrothing <andres.rodriguezhn@gmail.com>
2018-05-09 23:48:44 -0600

    Added image responsiveness

--- src/components/DepartmentPanel.jsx
+++ src/components/DepartmentPanel.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const DepartmentPanel = props => (
   <div className="container-department-panel">
-    <h1>{props.department.name}</h1>
-    <img src={props.department.imageURL} alt={props.department.name} />
-    <p>{props.department.description}</p>
+    <h1 className="container-department-panel-title">{props.department.name}</h1>
+    <img className="container-department-panel-img" src={props.department.imageURL} alt={props.department.name} />
+    <p className="container-department-panel-text">{props.department.description}</p>
   </div>
 );
 
